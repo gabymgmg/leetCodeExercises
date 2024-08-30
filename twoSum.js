aa5e64d25a2ac56b46257d20d503041c41c2861a
Maria Gabriela Martínez <gabrielamg@Marias-MacBook-Air.local>
2024-08-30 16:59:18 -0300

    two sum

---
+++ twoSum.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    let hashMap = new Map()
+    for(let i=0;i<nums.length;i++){
+        let temp = target-nums[i]
+        if(hashMap.has(temp)) return [i,hashMap.get(temp)]
+        hashMap.set(nums[i],i)
+    }
+    
+}; 
+
+
+console.log(twoSum([2,1,3,7,11,15],9))
