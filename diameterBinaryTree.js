f25dd8fcba01c818c0b77eccdbfff44438878c01
Maria Gabriela Martínez <gabrielamg@Marias-MacBook-Air.local>
2024-10-17 23:13:23 -0300

    diameter of a binary tree

---
+++ diameterBinaryTree.js
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+
+var diameterOfBinaryTree = function (root) {
+    if (root === null) return 0 // Handle empty tree
+    let max = 0; // keep the max value of the longest path
+
+    function getHeight(node) {
+        if (node === null) return 0 // when it reaches a leaf node
+
+        const leftHeight = getHeight(node.left);
+        const rightHeight = getHeight(node.right);
+        // diameter is calculated with the sum of l,r and node itself
+        max = Math.max(max, leftHeight + rightHeight); // Update max diameter 
+
+        return Math.max(leftHeight, rightHeight) + 1;
+    }
+    getHeight(root);
+    return max;
+}
